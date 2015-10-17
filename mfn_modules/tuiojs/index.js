508c95bcad34125ead42873ed4146c88cd65bf73
Didier C. Torres <didiercabrera@users.noreply.github.com>
2015-10-17 00:37:19 -0600

    Fixing errors in tuio.js

--- mfn_modules/tuiojs/index.js
+++ mfn_modules/tuiojs/index.js
@@ -1,13 +1,16 @@
 var util = require('util');
 var events = require('events');
-var OSC = require('../mfn_modules/oscjs');
+var OSC = require('../oscjs');
 
 function TUIO( port ){
 	var self = this;
 	this.port  = port || 3333;
+	this.cids = [];
 	var client = new OSC(self.port);
 	client.listen();
-	client.on('/tuio/2Dcur',process);
+	client.on('/tuio/2Dcur',function (data){
+		self.process(data);
+	});
 }
 util.inherits(TUIO, events.EventEmitter);
 
@@ -17,11 +20,10 @@ TUIO.prototype.process = function(data) {
 	
 	if (params[0] ==='alive'){
 		params.shift();
-
 		var removed_ids = [];
-		for (var i = 0; i < self.ids.length; i++) {
-			if( params.indexOf(self.ids[i]) ===-1 ){
-				removed_ids.push(self.ids[i]);
+		for (var i = 0; i < self.cids.length; i++) {
+			if( params.indexOf(self.cids[i]) ===-1 ){
+				removed_ids.push(self.cids[i]);
 			}
 		};
 
@@ -31,18 +33,18 @@ TUIO.prototype.process = function(data) {
 
 		var new_ids = [];
 		for (var i = 0; i < params.length; i++) {
-			if( self.ids.indexOf(params[i]) ===-1 ){
+			if( self.cids.indexOf(params[i]) ===-1 ){
 				new_ids.push(params[i]);
 			}
 		};
-		if (removed_ids.length > 0){
+		if (new_ids.length > 0){
 			self.emit('addCursors', new_ids );
 		}
-
-		self.ids = params;
+		self.cids = params;
 	}
 
 	if (params[0] ==='set'){
+		params.shift();
 		self.emit('updateCursor', {
 			'id':params[0],
 			'x':params[1],
@@ -51,3 +53,5 @@ TUIO.prototype.process = function(data) {
 	}
 	// body...
 };
+
+module.exports = TUIO;
